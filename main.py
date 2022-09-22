567f9f5ad1923e43bbb7a85b85000bfc72de5e9f
Jay Patel <61078385+jayparesh@users.noreply.github.com>
2022-09-21 20:37:44 -0400

    Add GET profile & UPDATE profile api endpoints

--- main.py
+++ main.py
@@ -54,3 +54,34 @@ def login(email, password):
             if i["email"] == email and i["password"] == password:
                 return {"Message": "Login Successfully"}
         raise HTTPException(status_code=401, detail="Login Error!!!")
+
+
+@app.get("/profile")
+def get_profile(email):
+    with open('user_cred.json', 'r+') as fd:
+        existing_data = json.load(fd)
+        for index, i in enumerate(existing_data["user_details"]):
+            if i["email"] == email:
+                return {
+                    "id": index,
+                    "data": i
+                }
+        raise HTTPException(status_code=401, detail="Profile not found!!!")
+
+
+@app.post("/profile")
+def update_profile(id, name, bio, phone, email, password):
+    try:
+        with open('user_cred.json', 'r+') as fd:
+            existing_data = json.load(fd)
+            data = existing_data["user_details"][id]
+            data["name"] = name
+            data["bio"] = bio
+            data["phone"] = phone
+            data["email"] = email
+            data["password"] = password
+            return {"Message": "User Profile Updated Successfully"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"{e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Profile Update Issue!!!")
